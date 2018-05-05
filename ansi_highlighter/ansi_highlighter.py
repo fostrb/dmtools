a469068534c58684cfce5fdb4f1390a2773d0270
fostrb <fostrb@gmail.com>
2018-05-05 16:25:24 -0400

    new data and highlighting.

---
+++ ansi_highlighter/ansi_highlighter.py
@@ -0,0 +1,75 @@
+import re
+from collections import OrderedDict
+
+# TODO: Regex isn't returning submatches.
+
+
+class AnsiHighlighter(object):
+    def __init__(self, highlight_dict, search_terms):
+        self.fg_highlights = highlight_dict
+        self.search_terms = search_terms
+
+    def build_fg_regex(self):
+        rstr = ''
+        for key, val in self.fg_highlights.items():
+            rstr += "(" + key + ")|"
+        rstr = rstr[:-1]
+        return rstr
+
+    def build_bg_regex(self):
+        rstr = ''
+        for key in self.search_terms:
+            rstr += "(" + key + ")|"
+        #rstr = rstr[:-1]
+        return rstr
+
+    def highlight(self, text):
+        bg_regex_str = self.build_bg_regex()
+        full_regex_str = str(bg_regex_str) + str(self.build_fg_regex())
+        bg_regex = re.compile(r'{}'.format(full_regex_str), re.I)
+
+        i = 0
+        h_output = ''
+
+        for m in bg_regex.finditer(text):
+            # print(m.group())
+            hl_prefix = ''
+            hl_suffix = ''
+            for match in m.groups():
+                if match is not None:
+                    # apply background
+                    for search in self.search_terms:
+                        if re.match(match, search, re.I):
+                            hl_prefix += '\x1b[7m'
+                            hl_suffix += '\x1b[27m'
+                    # apply foreground
+                    for fg_m, fg_v in self.fg_highlights.items():
+                        if re.match(match, fg_m, re.I):
+                            hl_prefix += '\x1b[38;5;' + self.fg_highlights[fg_m] + 'm'
+                            hl_suffix += '\x1b[39m'
+
+            h_output += "".join([text[i:m.start()], hl_prefix, text[m.start():m.end()], hl_suffix])
+            i = m.end()
+        if h_output != '':
+            return "".join([h_output, text[i:]])
+        else:
+            return text
+
+
+if __name__ == '__main__':
+    fg_highlights = OrderedDict({
+        'strength'      : '160',  # red
+        'dexterity'     : '190',  # yellow
+        'constitution'  : '100',  # brown-ish
+        'intelligence'  : '201',  # purple
+        'wisdom'        : '219',  # pink
+        'charisma'      : '51',  # blue
+    })
+
+
+    text = "strength is a thing that should be highlighted. Wisdom too. constitution intelligence"
+    search = ['str']
+    hl = AnsiHighlighter(fg_highlights, search)
+    out = hl.highlight(text)
+    print(out)
+
---
+++ newspellsearch.py
@@ -0,0 +1,297 @@
+import argparse
+import json
+import textwrap
+import re
+from random import randint
+from collections import OrderedDict
+
+from ansi_highlighter.ansi_highlighter import AnsiHighlighter
+
+# TODO: add filters for archetypes, domains, etc...
+# TODO: implement union/intersect/etc within class search/filter
+# TODO: print formatting update
+
+# TODO: add a switch for highlighted printing
+
+
+# to be used for highlighting searches
+SEARCH_TERMS = []
+
+# print flag: "n: just names, s: short"
+PRINT_FLAG = False
+
+
+HIGHLIGHTS = OrderedDict({
+    # abilities -------------------------------------
+    'strength'      : '196',  # red
+    'dexterity'     : '190',  # yellow
+    'constitution'  : '100',  # brown-ish
+    'intelligence'  : '201',  # purple
+    'wisdom'        : '219',  # pink
+    'charisma'      : '51',  # blue
+
+    # keywords
+    'saving throw'  : '203',  # salmon-ish
+
+    # schools of magic
+    # abjuration
+    # conjuration
+    # divination
+    # enchantment
+    # evocation
+    # illusion
+    # necromancy
+    # transmutation
+
+    # concentration
+    'concentration' : '39',  # sorta blue
+    # ritual
+
+
+    # dice regex
+    '\d+d\d+'          : '130',  # blue
+
+    # damage types (regex [optional 'damage'])
+    'psychic damage': '207',  # purple
+    'bludgeoning damage': '130',  # brown
+    'slashing damage': '130',  # orange
+    'piercing damage': '226',  # yellow
+    'fire damage': '160',  # red
+    'acid damage': '120',  # green
+    'cold damage': '123',  # light blue
+    'force damage': '95',  # weird brown
+    'lightning damage': '226',  # yellow
+    'thunder damage': '33',  # blue
+    'necrotic damage': '66',  # grey green
+    'poison damage': '82',  # green
+    'radiant damage': '15'  # white
+
+    # range regex: "touch"|number"feet"|other shit
+
+    # action regex: bonus, free, etc
+    # 1 action
+    # bonus action
+    # reaction
+
+    # Save DC
+    # Spell attack (melee, ranged, etc)
+})
+
+
+def filter_random(random, spells):
+    try:
+        r = int(random)
+        rlist = []
+
+        if r <= len(spells):
+            for i in range(r):
+                index = 0
+                while spells[index] not in rlist:
+                    index = randint(0, len(spells)-1)
+                    rlist.append(spells[index])
+            return rlist
+
+        else:
+            print("Length of previously filtered spells is less than " + str(r))
+            return spells
+    except AttributeError as e:
+        print(e)
+        exit(1)
+
+
+def filter_description(general, spells):
+    filtered_spells = []
+    filter_list = general.split(',')
+    for spell in spells:
+        if spell not in filtered_spells:
+            for search in filter_list:
+                if search in (spell['description']):
+                    filtered_spells.append(spell)
+    return filtered_spells
+
+
+def filter_classes(classes, spells):
+    filter_list = classes.split(',')
+    filtered_spells = []
+    for spell in spells:
+        get = True
+        if spell not in filtered_spells:
+            for search in filter_list:
+                regex = re.compile(r'{}'.format(search), re.I)
+                subget = False
+                for c in spell['classes']:
+                    if regex.match(c):
+                        subget = True
+                if not subget:
+                    get = False
+            if get:
+                filtered_spells.append(spell)
+    return filtered_spells
+
+
+def filter_gen(filters, spells):
+    filtered_spells = []
+    for spell in spells:
+        if spell not in filtered_spells:
+            get = True
+            for filter in filters:
+                if filter not in SEARCH_TERMS:
+                    SEARCH_TERMS.append(filter)
+                if filter not in str(spell):
+                    get = False
+            if get:
+                filtered_spells.append(spell)
+    return filtered_spells
+
+
+def filter_name(names, spells):
+    filter_list = names.split(',')
+    filtered_spells = []
+
+    for search in filter_list:
+        regex = re.compile(r'{}'.format(search), re.I)
+        for spell in spells:
+            if regex.match(spell['name']):
+                if spell not in filtered_spells:
+                    filtered_spells.append(spell)
+    return filtered_spells
+
+
+def filter_levels(levels, spells):
+    # generate a list of levels that matches the input level string(s)
+    filtered_spells = []
+    filter_list = levels.split(',')
+
+    for search in filter_list:
+        regex = re.compile(r'{}'.format(search), re.I)
+        for spell in spells:
+            if regex.match(str(spell['level'])):
+                if spell not in filtered_spells:
+                    filtered_spells.append(spell)
+    return filtered_spells
+
+
+def print_spell(spell):
+    name = spell['name']
+
+    components = spell['components']
+
+    is_verbal = False
+    is_somatic = False
+    is_material = False
+
+    if "V" in components:
+        is_verbal = True
+    if "S" in components:
+        is_somatic = True
+    if "M" in components:
+        is_material = True
+
+    raw_components = ",".join(components)
+
+    description = spell['description']
+    casting_time = spell['casting_time']
+    classes = spell['classes']  # list of strings
+
+    duration = spell['duration']
+    level = spell['level']
+    range = spell['range']
+    ritual = spell['ritual']  # bool
+    school = spell['school']
+    if PRINT_FLAG:
+        output = ''
+        #just names
+        if PRINT_FLAG == 'n':
+            output = name
+
+        #short
+        elif PRINT_FLAG == 's':
+            output += name + '\n'
+            output += "Casting Time: " + casting_time + '\n'
+    else:
+        output = name + '\n'
+        output += '-' * 80 + '\n'
+        output += textwrap.fill(raw_components, 80) + '\n'
+        output += textwrap.fill("Casting Time: " + casting_time, 80) + '\n'
+        output += textwrap.fill(description, 80) + '\n'
+        output += '-' * 80
+
+    return output
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # for now, pass a comma separated list and parse on your own
+    parser.add_argument("-i", '--infile', type=str, action='store', dest='infile',
+                        help='Import a json file of spells to work with instead of the entire list.')
+    parser.add_argument("-e", '--export', type=str, action='store', dest='export',
+                        help='Json dump query to a file.')
+    parser.add_argument("-c", "--class", type=str, action='store', dest='classes',
+                        help="Search class names.")
+    parser.add_argument("-n", "--names", type=str, action='store', dest='names',
+                        help="Search spell names")
+    parser.add_argument("-l", "--levels", type=str, action='store', dest='levels',
+                        help="Filter by spell level.")
+    parser.add_argument("-p", "--print", type=str, action='store', dest='print',
+                        help="Print formatting. 's'=short, 'n'=just names.")
+    parser.add_argument("-d", "--description", type=str, action='store', dest='description',
+                        help='Filter by description.')
+    parser.add_argument("-r", "--random", type=str, action='store', dest='random',
+                        help="Choose X spells from filtered list. This filter will run last.")
+    parser.add_argument("gen", default=[], nargs='*', action='store',
+                        help="Filter the spell's raw Json.")
+    args = parser.parse_args()
+    SPELLS = []
+    if not args.infile:
+        with open('data/better_spelldump.json') as json_data:
+            SPELLS = json.load(json_data)
+    else:
+        try:
+            with open(args.infile) as json_data:
+                SPELLS = json.load(json_data)
+        except:
+            print("issue with your file")
+            exit(1)
+    if args.print:
+        if args.print in ['n', 's']:
+            PRINT_FLAG = args.print
+    spells = SPELLS
+    if args.classes:
+        spells = filter_classes(args.classes, spells)
+    if args.names:
+        spells = filter_name(args.names, spells)
+    if args.levels:
+        spells = filter_levels(args.levels, spells)
+    if args.description:
+        spells = filter_description(args.general, spells)
+    if args.gen:
+        spells = filter_gen(args.gen, spells)
+
+    if args.random:
+        spells = filter_random(args.random, spells)
+    outputstr = ''
+
+    highlighter = AnsiHighlighter(HIGHLIGHTS, search_terms=SEARCH_TERMS)
+    for spell in spells:
+        outputstr += print_spell(spell) + '\n'
+    print(highlighter.highlight(outputstr))
+    print(str(len(spells)) + " spells matched criteria.")
+    if args.export:
+        try:
+            with open(args.export, 'w') as exportFile:
+                json.dump(spells, fp=exportFile)
+        except:
+            print("issue doing that")
+
+# search criteria:
+
+# spell is a ritual
+# class matches "string"
+# is_verbal/somatic/material
+# casting time: action, bonus, reaction, minute(s), hour(s)
+# duration
+# range
+# school matches string
+# does damage | average damage (requires parsing dice)
+# is AOE
