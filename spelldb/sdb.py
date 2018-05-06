e37672364332e588c274213e85d0430859380967
fostrb <fostrb@gmail.com>
2018-05-06 12:53:55 -0400

    field additions to database.

--- spelldb/sdb.py
+++ spelldb/sdb.py
@@ -8,37 +8,37 @@ import json
 # db = client.mongoengine_test
 
 
-
 class Spell(Document):
     name = StringField(required=True)
     description = StringField(required=True)
     classes = ListField(required=True)
     level = IntField(required=True)
-    higher_level = StringField(required=True)
+    higher_level = StringField(required=False)
     range = StringField(required=True)
+    components = ListField(required=True)
+    casting_time = StringField(required=True)
+
 
 db = connect('spelldb', host='localhost', port=27017)
 
-'''
 with open('better_spelldump.json') as fdata:
     spells = json.load(fdata)
-
-
 
 print(db)
 
 for spell in spells:
-    print(spell['range'])
-    print(spell['level'])
     # optionals here
     if 'higher_level' in spell.keys():
         higher_level = spell['higher_level']
     else:
-        higher_level = ''
+        higher_level = None
+
     if 'range' in spell.keys():
         sprange = spell['range']
     else:
-        sprange = ''
+        sprange = None
+
+
 
     s = Spell(
         name=spell['name'],
@@ -46,10 +46,12 @@ for spell in spells:
         classes=spell['classes'],
         level=spell['level'],
         higher_level=higher_level,
-        range=sprange
+        range=sprange,
+        components=spell['components'],
+        casting_time=spell['casting_time']
     )
     s.save()
-'''
+
 
 for s in Spell.objects:
     print(s.name)
