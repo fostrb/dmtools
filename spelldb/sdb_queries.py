ad6a1810065d92715c8e9f3fb873bdabec415414
fostrb <fostrb@gmail.com>
2018-05-06 23:14:51 -0400

    started to make regex functions for spelldb queries.

--- spelldb/sdb_queries.py
+++ spelldb/sdb_queries.py
@@ -1,27 +1,51 @@
 from pymongo import MongoClient
+import argparse
+from collections import OrderedDict
 import re
 
-# TODO: switch for query type (and/or/etc...)
+# TODO: levels are stored in db as integers. Makes regex matches a little less than fun. Custom search probably better.
+# TODO: flag for query type (and/or/etc...)
 
 
-def build_search(opts):
-    pass
-
-
-def intersection_search(search_objects):
-    stype = "$and"
+# this isn't what we want to use for classes. or pretty much anything but levels.
+def build_or_regex(opt_list):
+    regex_str = ''
+    for opt in opt_list:
+        regex_str += '(' + opt + ')|'
+    regex_str = regex_str[:-1]
+    regex = re.compile('{}'.format(regex_str), re.I)
+    return regex
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', type=str, nargs='*', action='store', dest='classes')
+    parser.add_argument('-l', type=str, nargs='*', action='store', dest='levels')
+
+    opts = []
+    args = parser.parse_args()
+
+    if args.classes:
+        cdict = {}
+        cdict['cclasses'] = build_or_regex(args.classes)
+        opts.append(cdict)
+
+    if args.levels:
+        print(args.levels)
+        ldict = {}
+        ldict['levels'] = build_or_regex(args.levels)
+        print(ldict)
+        opts.append(ldict)
+
     client = MongoClient('localhost', 27017)
-    #client = MongoClient()
+    # client = MongoClient()
     db = client.spelldb
     spells = db.spells
 
-    class_regex = re.compile('cleric', re.I)
-
+    # class_regex = re.compile('cleric', re.I)
     spells_returned = 0
-    for spell in spells.find({"$and": [{'cclasses': class_regex}, {'level': 0}]}):
+    # for spell in spells.find({"$and": [{'cclasses': class_regex}, {'level': 0}]}):
+    for spell in spells.find({"$and": opts}):
         spells_returned += 1
         print(spell['name'])
     print(spells_returned)
